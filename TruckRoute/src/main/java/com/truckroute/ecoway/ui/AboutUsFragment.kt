2e6eef33f28d6ad7ec54351f8e67678aeeb8208b
Rama Krishna <ramakrishna8.sunkara@gmail.com>
2022-02-18 11:18:25 +0530

    updated about us content

--- TruckRoute/src/main/java/com/truckroute/ecoway/ui/AboutUsFragment.kt
+++ TruckRoute/src/main/java/com/truckroute/ecoway/ui/AboutUsFragment.kt
@@ -1,11 +1,16 @@
 package com.truckroute.ecoway.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.tomtom.online.sdk.common.util.StringUtils
 import com.truckroute.ecoway.R
+import kotlinx.android.synthetic.main.fragment_about_us.*
 
 class AboutUsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +24,12 @@ class AboutUsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_about_us, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        textview_help_description.text = Html.fromHtml(getString(R.string.about_app), Html.FROM_HTML_MODE_COMPACT);
+        textview_help_description.movementMethod = LinkMovementMethod.getInstance();
     }
 
     companion object {
